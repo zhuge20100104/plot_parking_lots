9e8b63b2f9de7acaf45f0acff8f74fea2dad15f9
zhuge20100104 <Fredric2010@outlook.com>
2022-11-02 11:32:16 +0800

    add files

--- select_plots_and_car.py
+++ select_plots_and_car.py
@@ -11,11 +11,12 @@ ParkingLot = namedtuple("ParkingLot", ["p0", "p1", "p2", "p3"])
 
 def print_help():
     print("Please provide the parking lot type you want to select...")
-    print("Usage:  python select_plots_and_car.py  {parking_lot_type}")
+    print("Usage:  python select_plots_and_car.py  {parking_lot_type} {parking_lot_count}")
     print("Supported parking lot types:  [h, v, a]")
     print("h: For horizontal")
     print("v: For vertical")
     print("a: For angle")
+    print("parking_lot_count parameter is optional, default count is 3")
 
 def check_plot_type(plot_type):
     if plot_type != 'h' and plot_type != 'v' and plot_type != 'a':
@@ -25,21 +26,21 @@ def check_plot_type(plot_type):
     return True
 
 
-def show_select_park_lots(ix, iy, plot_type):
-    origin_point = (0, 0)
-    parking_lots_selected = list()
-    if plot_type == 'h':
-        #  水平车位大小 2.4 , 6  
-        iy_new = iy+2.4
-        ix_new = ix+6
+
+def show_select_car_mod(ix, iy, plot_type):
+    car_mod = list()
+    # Car 大小 [4.83, 2]
+    if plot_type == 'h': 
+        iy_new = iy+2
+        ix_new = ix+4.83
         p0 = (ix, iy)
         p1 = (ix, iy_new)
         p2 = (ix_new, iy_new)
         p3 = (ix_new, iy)
+
     elif plot_type == 'v':
-        # 垂直车位大小  5.4, 2.4
-        iy_new = iy+5.4
-        ix_new = ix+2.4
+        iy_new = iy+4.83
+        ix_new = ix+2
         p0 = (ix, iy)
         p1 = (ix, iy_new)
         p2 = (ix_new, iy_new)
@@ -51,10 +52,144 @@ def show_select_park_lots(ix, iy, plot_type):
         n = random.randint(3, 4)
         angle = math.pi/n
         p0 = (ix, iy)
+        p1 = (ix + 4.83*math.cos(angle), iy +  4.83* math.sin(angle))
+        p2 = (ix + 4.83*math.cos(angle) + 2, iy +  4.83* math.sin(angle))
+        p3 = (ix+ 2, iy)
+
+     
+    car_mod.append(p0)
+    car_mod.append(p1)
+    car_mod.append(p2)
+    car_mod.append(p3)
+
+    print("Current car MOD: ")
+    print(str(car_mod))
+
+    xy = conv_tuple_list_to_x_y_map(car_mod)
+    plt.scatter(xy.x, xy.y, s=POINT_SIZE , cmap=plt.cm.Spectral, alpha=0.8)
+    update_x_y_axis_and_replot()
+
+
+def  show_common_mod(ix, iy, width, height, mod_key) :
+    iy_new = iy+height
+    ix_new = ix+width
+    p0 = (ix, iy)
+    p1 = (ix, iy_new)
+    p2 = (ix_new, iy_new)
+    p3 = (ix_new, iy)
+
+    common_mod = list()
+
+    common_mod.append(p0)
+    common_mod.append(p1)
+    common_mod.append(p2)
+    common_mod.append(p3)
+    mod_type = MOD_NAME_MAP[mod_key]
+    print("Current "+ mod_type + " MOD: ")
+    print(str(common_mod))
+
+    xy = conv_tuple_list_to_x_y_map(common_mod)
+    plt.scatter(xy.x, xy.y, s=POINT_SIZE , cmap=plt.cm.Spectral, alpha=0.8)
+    update_x_y_axis_and_replot()
+
+
+def show_select_park_lots(ix, iy, plot_type):
+    origin_point = (0, 0)
+    parking_lots_selected = list()
+    
+    # Notice that this is an empty parking lots list  
+    parking_lots_and_theta_list = list()
+    if plot_type == 'h':
+        #  水平车位大小 2.4 , 6  
+        iy_new = iy+2.4
+        ix_new = ix+6
+
+        tan_x = ix + 6/2
+        tan_y = iy
+        p0 = (ix, iy)
+        p1 = (ix, iy_new)
+        p2 = (ix_new, iy_new)
+        p3 = (ix_new, iy)
+
+        for i in range(1, PARKING_LOT_COUNTS):
+            parking_lots_empty = list()
+            p0_1 =  (ix + i*(6 +0.2), iy)
+            p1_1 = (ix +  i*(6 +0.2), iy_new)
+            p2_1 = (ix +  i*(6+ 0.2)+6, iy_new)
+            p3_1 = (ix +  i*(6+ 0.2)+6, iy)
+            parking_lots_empty.append(p0_1)
+            parking_lots_empty.append(p1_1)
+            parking_lots_empty.append(p2_1)
+            parking_lots_empty.append(p3_1)
+
+            tan_x_1 = ix + i*(6 + 0.2) + 6/2
+            tan_y_1 = iy
+            thetas_empty = calculate_theta_and_opposite_theta(tan_x_1, tan_y_1)
+            pat = ParkingLotsAndTheta(parking_lots_empty, thetas_empty)
+            parking_lots_and_theta_list.append(pat)
+
+    elif plot_type == 'v':
+        # 垂直车位大小  5.4, 2.4
+        iy_new = iy+5.4
+        ix_new = ix+2.4
+
+        tan_x = ix + 2.4/2
+        tan_y = iy
+        p0 = (ix, iy)
+        p1 = (ix, iy_new)
+        p2 = (ix_new, iy_new)
+        p3 = (ix_new, iy)
+
+
+        for i in range(1, PARKING_LOT_COUNTS):
+            parking_lots_empty = list()
+            p0_1 =  (ix + i*(2.4 +0.2), iy)
+            p1_1 = (ix +  i*(2.4 +0.2), iy_new)
+            p2_1 = (ix +  i*(2.4+ 0.2)+2.4, iy_new)
+            p3_1 = (ix +  i*(2.4+ 0.2)+2.4, iy)
+     
+            parking_lots_empty.append(p0_1)
+            parking_lots_empty.append(p1_1)
+            parking_lots_empty.append(p2_1)
+            parking_lots_empty.append(p3_1)
+
+            tan_x_1 = ix + i*(2.4 + 0.2) + 2.4/2
+            tan_y_1 = iy
+            thetas_empty = calculate_theta_and_opposite_theta(tan_x_1, tan_y_1)
+            pat = ParkingLotsAndTheta(parking_lots_empty, thetas_empty)
+            parking_lots_and_theta_list.append(pat)
+    else:
+        # 倾斜车位大小和位置，这个比较复杂，得仔细看
+        # 7.99, 4.0
+        # 倾斜车位的角度在 30-60度   pi/6 -->  pi/3
+        n = random.randint(3, 4)
+        angle = math.pi/n
+        tan_x =  ix + 4/2
+        tan_y =  iy
+
+        p0 = (ix, iy)
         p1 = (ix + 8*math.cos(angle), iy +  8* math.sin(angle))
         p2 = (ix + 8*math.cos(angle) + 4, iy +  8* math.sin(angle))
         p3 = (ix+ 4, iy)
-    
+
+        
+        for i in range(1, PARKING_LOT_COUNTS):
+            parking_lots_empty = list()
+            p0_1 =  (ix + i*(4 +0.2/math.sin(angle)), iy)
+            p1_1 = (ix + i*(4 +0.2/math.sin(angle)) + 8*math.cos(angle), iy +  8* math.sin(angle))
+            p2_1 = (ix + i*(4 +0.2/math.sin(angle)) +  8*math.cos(angle) + 4,  iy +  8* math.sin(angle))
+            p3_1 = (ix + i*(4 +0.2/math.sin(angle)) + 4, iy)
+            parking_lots_empty.append(p0_1)
+            parking_lots_empty.append(p1_1)
+            parking_lots_empty.append(p2_1)
+            parking_lots_empty.append(p3_1)
+
+            tan_x_1 = ix + i*(4 +0.2/math.sin(angle)) + 4/2
+            tan_y_1 = iy
+            thetas_empty = calculate_theta_and_opposite_theta(tan_x_1, tan_y_1)
+            pat = ParkingLotsAndTheta(parking_lots_empty, thetas_empty)
+            parking_lots_and_theta_list.append(pat)
+     
     parking_lots_selected.append(p0)
     parking_lots_selected.append(p1)
     parking_lots_selected.append(p2)
@@ -62,28 +197,98 @@ def show_select_park_lots(ix, iy, plot_type):
     parking_lots_selected.append(origin_point)
     print("Current selected parking lot: ")
     print(str(parking_lots_selected))
-    
-    xy = conv_parking_slots_to_x_y_map(parking_lots_selected)
+
+    thetas = calculate_theta_and_opposite_theta(tan_x, tan_y)
+    print("Current two parking thetas: ")
+    print( thetas.theta)
+    print(thetas.opposite_theta)
+
+
+    print("================Start printing empty parking lots list=============================")
+    for pat in parking_lots_and_theta_list:
+        print("One==================")
+        parking_lots_emp = pat.parking_lot
+        thetas_emp = pat.theta
+
+        parking_lots_selected.extend(parking_lots_emp)
+        print("Current empty parking lots: ")
+        print(str(parking_lots_emp))
+        print("Current two empty thetas: ")
+        print(thetas_emp.theta)
+        print( thetas_emp.opposite_theta)
+        print("End One==================")
+        print("")
+        print("")
+    print("================End printing empty parking lots list=============================")
+
+    xy = conv_tuple_list_to_x_y_map(parking_lots_selected)
     # 清除之前的绘制
     plt.clf()
     plt.scatter(xy.x, xy.y, s=POINT_SIZE , cmap=plt.cm.Spectral, alpha=0.8)
-    plt.xlim((-20.0, 20.0))
-    plt.ylim((-20.0, 20.0))    
-    plt.xticks(range(-20, 20))
-    plt.yticks(range(-20, 20))
-    plt.show()
+    update_x_y_axis_and_replot()
+
+
+
+def show_select_uss_data_point(ix, iy,  plot_type):
+    near_far_points_selected = list()
+    if plot_type == 'h':
+        p0 = (ix, iy)
+        p1 = (ix+0.4, iy )
+    elif plot_type == 'v':
+        p0 = (ix, iy)
+        p1 = (ix, iy+0.4)
+    elif plot_type == 'a':
+        p0 = (ix, iy)
+        p1 = (ix + 0.2, iy + 0.2)
+    near_far_points_selected.append(p0)
+    near_far_points_selected.append(p1)
+
+    xy = conv_tuple_list_to_x_y_map(near_far_points_selected)
+    print("Current selected uss data points:")
+    print(str(near_far_points_selected))
+
+    plt.scatter(xy.x, xy.y, s=POINT_SIZE, cmap=plt.cm.Spectral, marker='x', alpha=0.8)
+
+    update_x_y_axis_and_replot()
+
 
 def select_parking_lot(plot_type):
-    fig = plt.figure()
+    fig = plt.figure(figsize=(12.8, 7.68))
     plt.xlim((-20.0, 20.0))
     plt.ylim((-20.0, 20.0))
     
-    def on_click_listener(event):
+    def on_select_parking_lot(event):
         ix, iy = event.xdata, event.ydata
         show_select_park_lots(ix, iy, plot_type)
         
+    def on_select_uss_data_point_or_mod(event):
+        if event.key == 'u':
+            # Start to select USS data points
+            ix, iy = event.xdata, event.ydata
+            show_select_uss_data_point(ix, iy, plot_type)
+        elif event.key == 'c':
+            # Start to select Car MOD
+            ix, iy = event.xdata, event.ydata
+            show_select_car_mod(ix, iy, plot_type)
+        elif event.key == 'p':
+            # Start to select pilliar
+            ix, iy = event.xdata, event.ydata
+            show_common_mod(ix, iy, 1.0, 1.1, 'p') 
+        elif event.key == 'o':
+            # Start to select Cone MOD
+            ix, iy = event.xdata, event.ydata
+            show_common_mod(ix, iy, 0.7, 0.38, 'o')  
+        elif event.key == 'r':
+            # Start to select person MOD
+            ix, iy = event.xdata, event.ydata
+            show_common_mod(ix, iy, 0.885, 0.44, 'r')  
+        elif event.key == 'b':
+            ix, iy = event.xdata, event.ydata
+            show_common_mod(ix, iy, 1.54, 0.76, 'b')  
 
-    fig.canvas.mpl_connect("button_press_event", on_click_listener)
+
+    fig.canvas.mpl_connect("button_press_event", on_select_parking_lot)
+    fig.canvas.mpl_connect("key_press_event", on_select_uss_data_point_or_mod)
     print(plot_type)
     plt.show()
 
@@ -91,7 +296,11 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print_help()
         exit(-1)
+    # 使用全局变量来传 PARKING_LOT_COUNTS 这个参数
+    global PARKING_LOT_COUNTS
     plot_type = sys.argv[1]
+    if len(sys.argv) == 3:
+        PARKING_LOT_COUNTS = int(sys.argv[2])
     
     if not check_plot_type(plot_type):
         exit(-1)
--- utils/funcs.py
+++ utils/funcs.py
@@ -1,9 +1,22 @@
 from collections import namedtuple
+import matplotlib.pyplot as plt
+import math
+
 XYs = namedtuple("XYs", ["x", "y"])
+Thetas = namedtuple("Thetas", ["theta", "opposite_theta"])
+ParkingLotsAndTheta = namedtuple("ParkingLotsAndTheta", ["parking_lot", "theta"])
 
 POINT_SIZE = 50
+PARKING_LOT_COUNTS = 3
+MOD_NAME_MAP = {
+    'c': "kCar",
+    'p': "kPillar",
+    'o': "kCone",
+    'r': "kPerson",
+    'b': "kBike"
+}
 
-def conv_parking_slots_to_x_y_map(ps_points):
+def conv_tuple_list_to_x_y_map(ps_points):
     x_list = list()
     y_list = list()
     for ps_point in ps_points:
@@ -11,3 +24,20 @@ def conv_parking_slots_to_x_y_map(ps_points):
         y_list.append(ps_point[1])
     xys = XYs(x_list, y_list)
     return xys
+
+def update_x_y_axis_and_replot():
+    plt.xlim((-20.0, 20.0))
+    plt.ylim((-20.0, 20.0))    
+    plt.xticks(range(-20, 20))
+    plt.yticks(range(-20, 20))
+    plt.show()
+
+
+def calculate_theta_and_opposite_theta(tan_x, tan_y):
+    theta = math.atan((float)(tan_y)/tan_x)
+
+    if theta < math.pi:
+        opposite_theta = math.pi + theta
+    else:
+        opposite_theta = theta  - math.pi
+    return Thetas(theta, opposite_theta)
